fc3a368a5f161249587cd3d849b4e480ce5032c1
Sergey Karavaev <sergeykaravaev44@gmail.com>
2023-08-05 17:53:51 -0700

    add 3 new components

---
+++ src/components/app-header/AppHeader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Logo} from '@ya.praktikum/react-developer-burger-ui-components'
+
+function AppHeader() {
+    return(
+        <Logo />
+    );
+}
+
+export default AppHeader;
