ff58d7801baa81b53367250ed8e803704725bc80
Sergey Karavaev <sergeykaravaev44@gmail.com>
2023-08-06 13:31:11 -0700

    add styles to burger ingredients

--- src/components/burger-ingredients/IngredientCard.tsx
+++ src/components/burger-ingredients/IngredientCard.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import {CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components'
+import "./BurgerIngredients.css"
 
 const IngredientCard = (props: { ingredient: any; }) => {
     const ingredient = props.ingredient;
 
     return(
-        <div key={ingredient._id}>
-            <img src={ingredient.image} alt={ingredient.name} />
-            <div>
-                <span>{ingredient.price}</span><CurrencyIcon type="primary" />
+        <div key={ingredient._id} className="pt-6 pb-8 card">
+            <img className="pl-4 pr-4" src={ingredient.image} alt={ingredient.name} />
+            <div className="pb-1 pt-1 card-icon-price">
+                <span className="text text_type_digits-default">{ingredient.price}</span><CurrencyIcon type="primary" />
             </div>
-            <h3>{ingredient.name}</h3>
+            <p className="text text_type_main-default">{ingredient.name}</p>
         </div>
     )
 }
