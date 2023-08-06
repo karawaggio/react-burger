b5af6e5c30dba637fa360fed0088048d7064f46e
Sergey Karavaev <sergeykaravaev44@gmail.com>
2023-08-06 00:50:28 -0700

    add semantical markup to AppHeader

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -5,31 +5,33 @@ import {BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-b
 
 function AppHeader() {
     return(
-    <div className="nav-container">
+    <header>
+        <nav className="nav-container">
 
-        <ul className="nav">
-            <li className="pl-5 pr-5 pb-4 pt-4 menu-item">
-                <BurgerIcon type="primary" />
-                <span className="pl-2 text text_type_main-default">Конструктор</span>
-            </li>
-            <li className="pl-5 pr-5 pb-4 pt-4 menu-item">
-                <ListIcon type="secondary" />
-                <span className="pl-2 text text_type_main-default">Лента заказов</span>
-            </li>
-        </ul>
-        
-        <div className="pb-4 pt-4 logo">
-            <Logo />
-        </div>
+            <ul className="nav">
+                <li className="pl-5 pr-5 pb-4 pt-4 menu-item">
+                    <BurgerIcon type="primary" />
+                    <span className="pl-2 text text_type_main-default">Конструктор</span>
+                </li>
+                <li className="pl-5 pr-5 pb-4 pt-4 menu-item">
+                    <ListIcon type="secondary" />
+                    <span className="pl-2 text text_type_main-default">Лента заказов</span>
+                </li>
+            </ul>
+            
+            <div className="pb-4 pt-4 logo">
+                <Logo />
+            </div>
 
-        <ul className="nav">
-            <li className="pl-5 pr-5 pb-4 pt-4 menu-item">
-                <ProfileIcon type="secondary" />
-                <span className="pl-2 text text_type_main-default pl-2 pr-2">Личный кабинет</span> 
-            </li>
-        </ul>
+            <ul className="nav">
+                <li className="pl-5 pr-5 pb-4 pt-4 menu-item">
+                    <ProfileIcon type="secondary" />
+                    <span className="pl-2 text text_type_main-default pl-2 pr-2">Личный кабинет</span> 
+                </li>
+            </ul>
 
-    </div>
+        </nav>
+    </header>
     );
 }
 
