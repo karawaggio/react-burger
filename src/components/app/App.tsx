5426aeef95aed21098a04addee8a873c9bf3b927
Sergey Karavaev <sergeykaravaev44@gmail.com>
2023-08-06 01:31:23 -0700

    import inhredients and constructor components

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppHeader from '../app-header/AppHeader'
+import BurgerIngredients from '../burger-ingredients/BurgerIngredients'
+import BurgerConstructor from '../burger-constructor/BurgerConstructor'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <AppHeader/>
+      <main>
+        <BurgerIngredients/>
+        <BurgerConstructor/>
+      </main>
     </div>
   );
 }
